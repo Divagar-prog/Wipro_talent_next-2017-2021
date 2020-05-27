84b63ac9aa8dc8e7d564c09c7f60d8e8597d9ee3
Divagar-prog <65938767+Divagar-prog@users.noreply.github.com>
2020-05-27 18:10:19 +0530

    DAY2

--- count.java
+++ count.java
@@ -5,17 +5,23 @@ public class Main
 	    int a[]=new int[30];
 	    int i=0;
 	    Scanner s=new Scanner(System.in);
-	    for(i=0;i<25;i++)
+	    for(i=0;i<10;i++)
 	    {
 	        a[i]=s.nextInt();
 	    }
 	    int max=a[i];
-	    for(i=0;i<25;i++)
+	    for(i=0;i<10;i++)
 	    {
 	        if(a[i]>max)
 	            max=a[i];
 	    }
-		System.out.println("Greatest no is");
-		System.out.println(max);
+	    int c=0;
+	    for(i=0;i<10;i++)
+	    {
+	        if(a[i]==max)
+	            c++;
+	    }
+		System.out.println("No of times max elment is :");
+		System.out.println(c);
 	}
 }
